e77c2f1e9a0309afdc68ae93277e481afbb5e2be
Nogrowth <anseong.jang@gmail.com>
2021-11-09 14:16:07 +0900

    Chapter 4-6

--- collector.py
+++ collector.py
@@ -16,7 +16,8 @@ class Collector:
 
     def db_setting(self, db_name, db_id, db_passwd, db_ip, db_port):
         print("db_setting 함수에 들어왔습니다.")
-        # mysql 데이터베이스랑 연동하는 방식.
+        # mysql 데이터베이스랑 연동하는 방식., 위에서 import한 sqlalchemy module 내의 create_engine method
+        # mysql 과 연동하는 방식인 create_engine!
         self.engine_bot = create_engine("mysql+mysqldb://" + db_id + ":" + db_passwd + "@"
                                         + db_ip + ":" + db_port + "/" + db_name, encoding='utf-8')
 
@@ -32,11 +33,11 @@ if __name__ == "__main__":
     # db_name 이라는 변수에 우리가 조회 하고자 하는 데이터베이스의 이름을 넣는다.
     db_name = 'bot_test1'
     # mysql db 계정
-    db_id = 'bot'
+    db_id = 'Nogrowth'
     # mysql db ip (자신의 PC에 DB를 구축 했을 경우 별도 수정 필요 없음)
     db_ip = 'localhost'  # localhost : 자신의 컴퓨터를 의미
     # mysql db 패스워드
-    db_passwd = '1234!@#$'
+    db_passwd = 'as6114'
     # db port가 3306이 아닌 다른 port를 사용 하시는 분은 아래 변수에 포트에 맞게 수정하셔야 합니다.
     db_port = '3306'
 
