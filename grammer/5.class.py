6c0d6826e67097fbe99eff0161e199af5c272d7a
Nogrowth <anseong.jang@gmail.com>
2021-11-09 10:37:52 +0900

    Chapter 4-4

--- grammer/5.class.py
+++ grammer/5.class.py
@@ -37,12 +37,13 @@ print("\n*************************************ex2*******************************
 #  tab 키로 띄워서
 class Stock:  # Stock 이라는 이름을 가진 클래스를 정의
     def __init__(self, stock_name, stock_price, stock_rate):  # __init__: '생성자'(특별한 함수) 라고 한다. 클래스의 객체를 만들 때 자동으로 실행이 된다.
+        # 매직 메서드라고도 하며, class 호출되면 자동으로 실행!! 디버깅에서도 안거치고 바로 들어간다.
         self.name = stock_name  # 인스턴스변수 : self.name, self.price, self.rate를 인스턴스 변수라고 한다.
         self.price = stock_price
         self.rate = stock_rate
 
 
-# item1, item2, item3은 객체 (=Stock이라는 클래스의 인스턴스)
+# item1, item2, item3은 객체 (=Stock이라는 클래스의 인스턴스!!!)
 # 삼성전자'를 Stock 클래스의 __init__ 생성자의 두번째 매개변수인 stock_name 전달.
 # 60900는 stock_price로 전달
 # 3.5는 stock_rate로 전달
@@ -61,7 +62,7 @@ print("\n*************************************ex3*******************************
 '''
 
 ** self ??
-    - class 안의 함수들은 첫 매개변수(=parameter) 로 self 를 사용
+    - class 안의 함수들은 첫 매개변수(=parameter) 로 self 를 사용!!
     - self는 해당 클래스의 객체 자신.  
     - 객체를 호출할 때 호출한 객체 자신이 전달되기 때문에 self를 사용한 것
 '''
@@ -79,7 +80,9 @@ class Stock2:  # Stock2 이라는 이름을 가진 클래스를 정의
 item1 = Stock2('삼성전자', 60900, 3.5)
 
 # id() 내장 함수는 객체를 입력값으로 받아서 객체의 고유값(일련번호)을 반환하는 함수
-# 출력 결과 self의 일련번호와 동일
+# 출력 결과 self의 일련번호와 동일!! 단, item1.name, price 등의 id는 다르다..
+# self는 instance 자기 자신이다!!!
+
 print(f"item1 객체의 일련번호 : {id(item1)}\n")  # \n을 추가하면 한 줄 띄어서 출력
 
 item2 = Stock2('SK텔레콤', 238000, 20)
@@ -96,7 +99,7 @@ print("\n*************************************ex4*******************************
 객체: 클래스의 정의로 생성된 대상이다. 
 
 객체의 특징인 속성은 변수로, 객체가 할 수 있는 일인 기능은 메서드로 구현되어있다.
-즉, 객체는 클래스의 정의대로 변수와 메서드의 묶음으로 이루어져있다.
+즉, 객체는 클래스의 정의대로 변수와 메서드!!!의 묶음으로 이루어져있다.
 
 ex1) 객체가 주식종목 이라면 stock_name(종목명), price(현재가), rate(수익률)과 같은 속성은 변수로 구현하고
 print(출력), change_price(가격 변경)과 같은 기능(동작)은 메서드로 구현
@@ -120,6 +123,7 @@ class Stock3:
     def change_price(self, new_price):
         self.price = new_price
 
+# 클래스 내부의 메서드를 호출할 때에도 항상 instance명으로 호출한다!!
 
 item1 = Stock3('삼성전자', 60900, 3.5)
 item2 = Stock3('SK텔레콤', 238000, 20)
@@ -192,7 +196,7 @@ __init__에 매개변수가 없을 경우?
 
 
 class Ex6:
-    def __init__(self):
+    def __init__(self): # 생성자의 self parameter 빼고 필수는 아니다!
         # self.x, self.y  : instance 변수
         self.x, self.y = 1, 2
 
